0d6e93a21c5634d855070406c99f6a85256333eb
Dan Pollack <dpollack@ds-science.com>
2020-07-15 17:08:39 -0400

    updated parsing to use new DA folder-deviations-gallery-XXXXXXX format for encoding gallery ID number

--- da-feed-link.py
+++ da-feed-link.py
@@ -2,19 +2,19 @@ import sys
 import re
 import urllib.request
 
-
+# when updating need to look for folder-deviations-gallery-XXXXXXXX
 uname = sys.argv[1]
 url = 'https://www.deviantart.com/' + uname + '/gallery/'
 html = urllib.request.urlopen(url)
 doc = html.read().decode()
 parts = doc.split(",")
 for each in parts:
-    if re.search(r'featured"', each):
-        tags  = each.split("<")
+    if re.search(r'folder-deviations-gallery', each):
+        tags  = each.split(":")
 for item in tags:
-    if re.search(r'featured"', item):
-        urlparts = item.split("/")
+    if re.search(r'folder-deviations-gallery', item):
+        urlparts = item.split("-")
 
-galleryid = urlparts[5]
+galleryid = urlparts[3][:-2]
 feedurl = "http://backend.deviantart.com/rss.xml?q=gallery:" + uname + "/" + galleryid + "&type=deviation"
 print (feedurl)
